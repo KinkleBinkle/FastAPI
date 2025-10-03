5ff4a32471e436a5bbb4184a01bc1177428e28d3
mamoon-17 <mamoonmasood17@gmail.com>
2025-10-03 19:05:43 +0500

    pull

--- 1. setup/backend/main.py
+++ 1. setup/backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends
 from fastapi.concurrency import asynccontextmanager
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from typing import List, Optional
 from database import engine, get_db
 from models import Base, Book
 from schemas import bookResponse, bookCreate
@@ -20,7 +21,7 @@ app = FastAPI(lifespan=lifespan)
 async def read_root():
     return{"message": "Books API - Module 1"}
 
-app.get("/books")
+@app.get("/books", response_model=list[bookResponse])
 async def list_books(db: AsyncSession = Depends(get_db)):
     result = await db.execute(select(Book))
     books = result.scalars().all()
